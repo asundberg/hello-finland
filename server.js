95079f6f2cc9743828671100824b826cf2a2bc3d
Annika Sundberg <annika.m.h.sundberg@gmail.com>
2016-08-07 22:17:37 -0400

    Adjusted build folder

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + './build'));
 
 app.listen(process.env.PORT || 8080);
+
+// before pushing to heroku:
+// first npm run build
+// then git commit + push
+// then git push heroku master
